d1cfc6eb9a6c03b16e97f0a9fc1f36ba6911a8fe
DanielMagirias <danielm12cs@gmail.com>
2025-09-11 12:24:19 -0400

    Made a "Ground" with a line or rectangle version

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -12,6 +12,7 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 const unsigned int TARGET_FPS = 50;// frames/second
 float dt = 1.0f / TARGET_FPS; // seconds/frame
 float time = 0;
+
 float x = 500;
 float y = 500;
 float frequency = 1;
@@ -23,23 +24,25 @@ void update()
     dt = 1.0f / TARGET_FPS;
     time += dt;
 
-    x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
-    y = y + (cos(time * frequency)) * frequency * amplitude * dt;
+    //x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+    //y = y + (cos(time * frequency)) * frequency * amplitude * dt;
 }
 
 void draw() 
 {
         BeginDrawing();
             ClearBackground(WHITE);
-            DrawText("Daniel Magirias 101552396", 10, GetScreenHeight() - 30, 20, BLACK);
-
 
             GuiSliderBar(Rectangle{ 60, 50, 1000, 50 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
             DrawText(TextFormat("Time: %.2f", time), GetScreenWidth() / 2 - 75, 10, 30, LIGHTGRAY);
 
-            DrawCircle(x, y, 70, RED);
-            DrawCircle(500 + cos(time * frequency)* amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);
+            //DrawCircle(x, y, 70, RED);
+            //DrawCircle(500 + cos(time * frequency)* amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN); For the Moving Circles
 
+            //DrawLine(0, 700, 1200, 700, GREEN); //Ground (line version)
+            DrawRectangle(0, 700, 1200, 100, GREEN); // Ground (Rectangel version)
+
+            DrawText("Daniel Magirias 101552396", 10, GetScreenHeight() - 30, 20, BLACK);
 
         EndDrawing();
 }
